509a1c627eba9e6c46017895b43d6cda5f50bf2f
Marco Kang <kmnkit@gmail.com>
2022-01-28 20:31:14 +0900

    Feedback 反映

--- assignment-5/ViewController.swift
+++ assignment-5/ViewController.swift
@@ -10,40 +10,29 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - Properties
-    private var leftNumber: Float?
-    private var rightNumber: Float?
-        
     @IBOutlet private weak var leftNumberField: UITextField!
     @IBOutlet private weak var rightNumberField: UITextField!
     
     @IBOutlet private weak var resultLabel: UILabel!
         
     // MARK: - Actions
-    @IBAction func editLeftNumberField(_ sender: UITextField) {
-        // 左のTextFieldの編集が終わったらleftNumberに値をセットする
-        leftNumber = Float(sender.text ?? "") ?? nil
-    }
-    
-    @IBAction func editRightNumberField(_ sender: UITextField) {
-        // 右のTextFieldの編集が終わったらrightNumberに値をセットする
-        rightNumber = Float(sender.text ?? "") ?? nil
-    }
-    
     @IBAction func tapDivideButton(_ sender: UIButton) {
         // 左や右のテキストフィールドにカーソルが合っているままだとボタンを押したときに入力終了判定にならないため、
         // 入力強制終了させる。
         leftNumberField.endEditing(true)
         rightNumberField.endEditing(true)
         
+        let leftNumber = Float(leftNumberField.text ?? "")
+        let rightNumber = Float(rightNumberField.text ?? "")
         guard let leftDivideNumber = leftNumber else {
-            showAlert(message: "左に正しく分子を入力してください")
-            return
-        }
+                    showAlert(message: "左に正しく分子を入力してください")
+                    return
+                }
         guard let rightDivideNumber = rightNumber else {
             showAlert(message: "割る数を入力してください")
-            return
+                        return
         }
-        if rightDivideNumber == 0 {
+        guard rightDivideNumber != 0 else {
             showAlert(message: "割る数には０を入力しないでください")
             return
         }
@@ -55,8 +44,7 @@ class ViewController: UIViewController {
         let alert = UIAlertController(title: "課題5",
                                       message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-        return
+        present(alert, animated: true, completion: nil)
     }
 }
 
